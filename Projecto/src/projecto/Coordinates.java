a043f6d2eadaeb2fe9bc5ce2a04924f31514f2dd
Francisco Magalhães <franciscomags@gmail.com>
2015-01-13 21:45:40 +0000

    javadoc
    javadoc changes

--- Projecto/src/projecto/Coordinates.java
+++ Projecto/src/projecto/Coordinates.java
@@ -1,33 +1,48 @@
 package projecto;
 
+/** Classe com a posição de uma entidade*/
+
 public class Coordinates {
     private int x;
     private int y;
 
-    //Construtctors
+   /**Construtor com a iniciação das variáveis
+    * @param x
+    * @param y 
+    */
     public Coordinates(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    //Set's
+    /**Método responsável pela introdução da coordenada X de uma entidade..
+     * @param x.*/
     public void setX(int x) {
         this.x = x;
     }
 
+    /**Método responsável pela introdução da coordenada Y de uma entidade..
+     * @param y.*/
     public void setY(int y) {
         this.y = y;
     }
     
-    //Get's
+    /**Método responsável pelo retorno da coordenada X de uma entidade.
+     * @return x
+     */
     public int getX() {
         return x;
     }
 
+    /**Método responsável pelo retorno da coordenada Y de uma entidade.
+     * @return y
+     */
     public int getY() {
         return y;
     }
 
+    /**Método responsável pelo retorno de uma string sempre que existe a tentativa de impressão da classe.
+     * @return String*/
     @Override
     public String toString() {
         return "(" + x + ", " + y + ')';
--- Projecto/src/projecto/Entity.java
+++ Projecto/src/projecto/Entity.java
@@ -1,12 +1,18 @@
 package projecto;
 
+/** Classe para objectos do tipo entidade, onde serão contidos todos os valores necessários no contexto da simulação*/
+
 public abstract class Entity {
     protected String id;
     protected String shape;
     protected String color;
     protected Coordinates coords;
     
-    //Construtctors
+    /**Construtor com iniciação de todas as variávei
+     * @param id
+     * @param shape
+     * @param color
+     * @param coords*/
     public Entity(String id, String shape, String color, Coordinates coords) {
         this.id = id;
         this.shape = shape;
@@ -14,45 +20,66 @@ public abstract class Entity {
         this.coords = coords;
     }
     
-    //Set's
+    /**Método responsável pela introdução do ID de uma entidade..
+     * @param id.*/
     public void setId(String id) {
         this.id = id;
     }
 
+    /**Método responsável pela forma de uma entidade..
+     * @param shape.*/
     public void setShape(String shape) {
         this.shape = shape;
     }
 
+    /**Método responsável pela introdução da cor de uma entidade..
+     * @param color.*/
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**Método responsável pela introdução das coordenadas de uma entidade..
+     * @param coords.*/
     public void setCoords(Coordinates coords) {
         this.coords = coords;
     }
     
-    //Get's
+     /**Método responsável pelo retorno do ID de uma entidade.
+     * @return id
+     */
     public String getId() {
         return id;
     }
 
+    /**Método responsável pelo retorno da forma de uma entidade.
+     * @return shape
+     */
     public String getShape() {
         return shape;
     }
 
+    /**Método responsável pelo retorno da cor de uma entidade.
+     * @return color
+     */
     public String getColor() {
         return color;
     }
 
+    /**Método responsável pelo retorno das coordenadas de uma entidade.
+     * @return coords
+     */
     public Coordinates getCoords() {
         return coords;
     }
 
-    @Override
+    /**Método responsável pelo retorno de uma string sempre que existe a tentativa de impressão da class
+     * @return string*/
     public String toString() {
         return "Id: " + id + ", Forma: " + shape + ", Cor: " + color + ", Coordenadas: " + coords;
     }
     
+    /**Método responsável pelo retorno de uma string sempre que existe a tentativa de impressão da class
+     * @return string*/
     public String toString(int i) {
         return "\nId: " + id + "\nForma: " + shape + "\nCor: " + color;
     }
